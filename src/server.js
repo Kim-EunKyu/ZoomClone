cddd06ad95a35dd10a7eb0dea5e128fb9c0730b6
Kim-EunKyu <rladmsrb96@naver.com>
2021-08-18 21:55:44 +0900

    conf: frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, () => {
+  console.log(`Listening on 3000`);
+});
